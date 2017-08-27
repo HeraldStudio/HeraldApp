458b1f8635aad6cb6007aa4e6f159ed18be99ef6
Vhyme <vhyme@live.cn>
2017-08-27 15:48:18 +0800

    x

--- static/js/config.js
+++ static/js/config.js
@@ -3,7 +3,7 @@ angular.module('HeraldApp.config',[])
     "name":"production",
     "debug":true,
     "version":'1.0.0',
-    "api":'http://www.heraldstudio.com/herald/api/v1'
+    "api":'//heraldstudio.com/herald/api/v1'
 })
 .config(['$ionicConfigProvider',function($ionicConfigProvider){
     $ionicConfigProvider.tabs.position('bottom');  //默认ios tabs在上面显示，这里做全局设置，tabs在下面设置，无论是android还是ios
